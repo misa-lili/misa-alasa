0ed9c7331abe5dce3f3fd85a773fe70f48065390
adt-hgkim <hgkim@gnmpm.com>
2023-07-03 22:01:29 +0900

    feat: test 🚧

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,21 @@
 	});
 
 	const transcode = async ({ target: { files } }) => {
-		const ffmpeg = createFFmpeg({ log: true });
+		const ffmpeg = createFFmpeg();
+		ffmpeg.setLogger(({ type, message }) => {
+			console.log({ type, message });
+			const textarea = window.document.getElementById('text19');
+			textarea.value += `${type}: ${message}\n`;
+			textarea.scrollTop = textarea.scrollHeight;
+		});
 		const { name } = files[0];
 		await ffmpeg.load();
 		ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
 		await ffmpeg.run(
 			'-i',
 			name,
+			'-row-mt',
+			'1',
 			'-r',
 			'30',
 			'-t',
@@ -24,23 +32,29 @@
 			'libvpx-vp9',
 			'-pix_fmt',
 			'yuva420p',
-			'-s',
-			'512x512',
-			'-row-mt',
-			'1',
+			'-vf',
+			'scale=512:512',
+			'-b:v',
+			'256k',
 			'output.webm'
 		);
 		const data = ffmpeg.FS('readFile', 'output.webm');
-		const video = document.getElementById('player');
-		const source = document.createElement('source');
-		const url = URL.createObjectURL(new Blob([data.buffer]));
-		source.setAttribute('src', url);
-		source.setAttribute('type', 'video/webm');
+		const blob = new Blob([data.buffer], { type: 'video/webm' });
+		// const video = window.document.querySelector('video#player');
 
-		video.appendChild(source);
-		video.load();
-		video.play();
-		clickFunction(url);
+		// const source = video?.querySelector('source');
+		// const url = window.URL.createObjectURL(blob);
+
+		// source.setAttribute('src', url);
+		// video.load();
+		// video
+		// 	.play()
+		// 	.then(
+		// 		(ful) => console.log(ful),
+		// 		(rej) => console.log(rej)
+		// 	)
+		// 	.catch((err) => console.log(err));
+		// clickFunction(url);
 	};
 
 	function clickFunction(url) {
@@ -55,7 +69,7 @@
 
 <div class="window container" style="width:280px;">
 	<div class="title-bar">
-		<div class="title-bar-text">A Window With Stuff In It</div>
+		<div class="title-bar-text">텔레그램 스티커 생성기</div>
 		<div class="title-bar-controls">
 			<button aria-label="Minimize" />
 			<button aria-label="Maximize" />
@@ -64,17 +78,17 @@
 	</div>
 	<div class="window-body" style="overflow:hidden;">
 		<menu role="tablist">
-			<li role="tab" aria-selected="false"><a href="#tabs">직접업로드</a></li>
+			<!-- <li role="tab" aria-selected="false"><a href="#tabs">직접업로드</a></li> -->
 			<li role="tab" aria-selected="true"><a href="#tabs">아카콘</a></li>
-			<li role="tab" aria-selected="false"><a href="#tabs">디시콘</a></li>
+			<!-- <li role="tab" aria-selected="false"><a href="#tabs">디시콘</a></li>
 			<li role="tab" aria-selected="false"><a href="#tabs">노벨콘</a></li>
-			<li role="tab" aria-selected="false"><a href="#tabs">라인콘</a></li>
+			<li role="tab" aria-selected="false"><a href="#tabs">라인콘</a></li> -->
 		</menu>
 		<div class="window" role="tabpanel">
 			<div class="window-body">
 				<p>어디 한번 잘 해보세요!</p>
 				<ul>
-					<li>움직이는 스티커가 한 장이라도 포함된 경우 스티커 셋의 최대 크기는 50개 입니다.</li>
+					<li>스티커 셋의 최대 크기는 50개 입니다.</li>
 					<li>품질을 조절해서 스티커의 용량을 조절해주세요.</li>
 					<li>움직이는 스티커의 경우 256kb가 최대입니다.</li>
 				</ul>
@@ -82,17 +96,22 @@
 		</div>
 
 		<br />
+
 		<div class="field-row">
-			<label for="text17">Occupation</label>
+			<label for="text17">아카콘 번호</label>
 			<input id="text17" type="text" />
 		</div>
 
+		<br />
+
 		<div class="field-row" style="width: 100%">
-			<label for="range25">품질</label>
+			<label for="range26">품질</label>
 			<label for="range26">64k</label>
 			<input id="range26" type="range" min="1" max="11" value="5" />
-			<label for="range27">512k</label>
+			<label for="range26">512k</label>
 		</div>
+
+		<br />
 
 		<div class="field-row">
 			<input id="radio5" type="radio" name="first-example" checked />
@@ -103,7 +122,7 @@
 			<label for="radio6">로그 채널에 공개</label>
 		</div>
 
-		<video id="player" controls width="100%" />
+		<br />
 
 		<input type="file" id="uploader" />
 		<div style="display: flex; width: 100%;">
@@ -111,6 +130,16 @@
 			<input style="flex:1;" type="reset" />
 		</div>
 
+		<br />
+
+		<div class="field-row-stacked" style="width: 100%">
+			<label for="text19">인코딩 로그</label>
+			<textarea id="text19" rows="8" value="" readonly />
+		</div>
+
+		<br />
+
+		hihi
 		<div class="sunken-panel" style="height: 120px; width: 100%;">
 			<table class="interactive">
 				<thead>
@@ -175,8 +204,20 @@
 			</table>
 		</div>
 		<div class="field-row-stacked" style="width: 100%">
-			<label for="text20">Additional notes</label>
-			<textarea id="text20" rows="8" />
+			<label for="text20">@misa-alasa 신규 스티커 작성 코드</label>
+			<textarea
+				id="text20"
+				rows="8"
+				value="/newvideo 12351235,43623462346,54856856,13452345234,632346234,12351253"
+			/>
+		</div>
+		<div class="field-row-stacked" style="width: 100%">
+			<label for="text20">@misa-alasa 스티커 추가 작성 코드</label>
+			<textarea
+				id="text21"
+				rows="8"
+				value="/addsticker misa-alasa-arca-1234 12351235,43623462346,54856856,13452345234,632346234,12351253"
+			/>
 		</div>
 	</div>
 </div>
