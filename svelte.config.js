5e53e6095ff684130b43cebfc2f1ed845dbe1ec4
adt-hgkim <hgkim@gnmpm.com>
2023-07-05 00:59:49 +0900

    feat: cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,15 +12,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: true,
-			strict: false
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
 
